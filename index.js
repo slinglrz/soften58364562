25bfae1c01fae7b890a974cf103ad224175cb64a
slinglrz <35695532+slinglrz@users.noreply.github.com>
2018-10-12 16:09:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const port = 8000;
 
+app.use(express.static('public'))
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log('app listening on port'+port))
